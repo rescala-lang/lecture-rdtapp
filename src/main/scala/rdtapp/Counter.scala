39ddc96a884ce72a02d6e0f9fee120b95af75255
ragnar <git@rmgk.de>
2024-09-08 16:53:50 +0200

    some slidy thingies

--- src/main/scala/rdtapp/Counter.scala
+++ src/main/scala/rdtapp/Counter.scala
@@ -2,18 +2,13 @@ package rdtapp
 
 import rdts.base.{Bottom, Lattice, LocalUid, Uid}
 
-/** Integers are not a great choice to represent a replicated value, as none of the straightforward merge functions seems semantically useful */
 object IntegerLattices {
 
-  /** Bad because if two replicas increment, only the larger increment wins. */
-  given max: Lattice[Int] = math.max
-
-  /** Same as max, but in the other direction */
-  given min: Lattice[Int] = math.min
-
-  /** Bitwise operations really make no sense to use as counters */
+  given max: Lattice[Int]        = math.max
+  given min: Lattice[Int]        = math.min
   given bitwiseOr: Lattice[Int]  = (x, y) => x | y
   given bitwiseAnd: Lattice[Int] = (x, y) => x & y
+
 }
 
 /** Core idea: Per replica counters */
@@ -23,37 +18,57 @@ case class GrowOnlyCounter(entries: Map[Uid, Int]) {
     * To make replication more efficient, the new value only contains the changed part of the state (the updated entry)
     */
   def add(n: Int)(using replicaId: LocalUid): GrowOnlyCounter =
-    val current = entries.getOrElse(replicaId.uid, 0)
-    GrowOnlyCounter(Map(replicaId.uid -> (current + n)))
+    val currentValue   = entries.getOrElse(replicaId.uid, 0)
+    val increasedValue = currentValue + n
+    GrowOnlyCounter(Map(replicaId.uid -> increasedValue))
 
   /** Get the semantic value of the counter, by summing all the individual counts at each replica! */
   def value: Int = entries.values.sum
 }
 
 object GrowOnlyCounter {
+
   import rdtapp.IntegerLattices.max
+
   given Lattice[GrowOnlyCounter] = Lattice.derived
 
   given Bottom[GrowOnlyCounter] = Bottom.provide(zero)
 
   def zero: GrowOnlyCounter = GrowOnlyCounter(Map.empty)
+}
+
+object Examples {
+
+  def test() = {
+
+    given Lattice[Int] = math.max
+
+    val x: 10 = 10
+
+    val delta1: Int = x + 2
+
+    val delta2: Int = x + 5
+
+    val result: Int = delta1.merge(delta2)
+
+  }
 
   /** main method just for example purposes */
   def main(args: Array[String]): Unit = {
 
-    val start = GrowOnlyCounter.zero
+    val start = GrowOnlyCounter.zero.add(10)(using Uid.zero)
 
     val r1 = LocalUid.gen()
     val r2 = LocalUid.gen()
 
     val delta1 = {
       given LocalUid = r1
-      start.add(10)
+      start.add(2)
     }
 
     val delta2 = {
       given LocalUid = r2
-      start.add(11)
+      start.add(5)
     }
 
     val result = start.merge(delta1).merge(delta2)
